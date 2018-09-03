a50119515e9de6857aa5a473fa6edcd228cfd27d
Mihir Amin <mihiramin89@gmail.com>
2018-09-03 01:56:08 -0400

    basic fetch calls to scos

--- server.js
+++ server.js
@@ -1,12 +1,74 @@
 var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000;
+    app = express(),
+    port = process.env.PORT || 3000;
+const request = require('request');
+
 
 app.get('/pantries', function(req, res) {
-    res.send([{name:'panty1'}, {name:'pantry2'}]);
+    const options = {
+        // body: {
+        //     resource_id: '570a8e02-fb0e-4cee-895b-3b32bd740650', // the resource id
+        //     limit: 5, // get 5 results
+        //     q: 'jones' // query for 'jones'
+        // },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        json: true,
+        method: 'GET',
+        uri: 'https://ckan.smartcolumbusos.com/api/action/datastore_search?resource_id=570a8e02-fb0e-4cee-895b-3b32bd740650&limit=100',
+    };
+    request(options, function(error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+        res.send(body.result.records);
+    });
 });
+
+app.get('/pantries/:limit', function(req, res) {
+    const options = {
+        // body: {
+        //     resource_id: '570a8e02-fb0e-4cee-895b-3b32bd740650', // the resource id
+        //     limit: 5, // get 5 results
+        //     q: 'jones' // query for 'jones'
+        // },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        json: true,
+        method: 'GET',
+        uri: `https://ckan.smartcolumbusos.com/api/action/datastore_search?resource_id=570a8e02-fb0e-4cee-895b-3b32bd740650&limit=${req.params.limit}`,
+    };
+    request(options, function(error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+        res.send(body.result.records);
+    });
+});
+
 app.get('/pantries/:zipCode', function(req, res) {
-    res.send({zipCode:req.params.zipCode, name: "The Name", description: "description"});
+    const options = {
+        body: {
+            // resource_id: '570a8e02-fb0e-4cee-895b-3b32bd740650', // the resource id
+            // limit: 5, // get 5 results
+            // q: '43215' // query for 'jones'
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        json: true,
+        method: 'GET',
+        uri: `https://ckan.smartcolumbusos.com/api/action/datastore_search?resource_id=570a8e02-fb0e-4cee-895b-3b32bd740650&limit=100&q=${req.params.zipCode}`,
+    };
+    request(options, function(error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+        res.send(body.result.records);
+    });
+    // res.send({ zipCode: req.params.zipCode, name: "The Name", description: "description" });
 });
 
 app.listen(port);
