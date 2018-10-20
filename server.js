3dfc2f0a1df25552f21a72a544ee807d0eeb5bc7
Mihir Amin <mihiramin89@gmail.com>
2018-10-20 12:19:17 -0400

    all pantries (with paging), all pantries in zipcode (with paging)

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000;
 const request = require('request');
+const host = 'https://ckan.smartcolumbusos.com/';
+const resourceId = '570a8e02-fb0e-4cee-895b-3b32bd740650';
 
-
+// NOTE: query returns max of 100. to get next 100 we need to do the following query
+// '/api/action/datastore_search?offset=100&resource_id=570a8e02-fb0e-4cee-895b-3b32bd740650'
 app.get('/pantries', function(req, res) {
+    const limit = req.query.limit || 100; // query will only return 100 results.
+    const pageNumber = req.query.pageNumber || 1;
+
+    let uri = `${host}api/action/datastore_search?resource_id=${resourceId}&limit=${limit}`;
+    if (pageNumber > 1) {
+        const offset = limit * (pageNumber - 1);
+        uri = `${host}api/action/datastore_search?offset=${offset}&resource_id=${resourceId}&limit=${limit}`;
+    }
+
     const options = {
         body: {},
         headers: {
@@ -12,17 +24,31 @@ app.get('/pantries', function(req, res) {
         },
         json: true,
         method: 'GET',
-        uri: 'https://ckan.smartcolumbusos.com/api/action/datastore_search?resource_id=570a8e02-fb0e-4cee-895b-3b32bd740650&limit=100',
+        uri,
     };
     request(options, function(error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body);
+        if (error) {
+            console.log('error:', error); // Print the error if one occurred
+        }
+
+        // console.log('body:', body);
         res.send(body.result.records);
     });
 });
-
+// NOTE: query returns max of 100. to get next 100 we need to do the following query
+// '/api/action/datastore_search?offset=100&resource_id=570a8e02-fb0e-4cee-895b-3b32bd740650'
 app.get('/pantries/:zipCode', function(req, res) {
+    const limit = req.query.limit || 100;
+    const zipCode = req.params.zipCode;
+    const pageNumber = req.query.pageNumber || 1;
+
+    let uri = `${host}api/action/datastore_search?resource_id=${resourceId}&limit=${limit}&q=${zipCode}`;
+    if (pageNumber > 1) {
+        const offset = limit * (pageNumber - 1);
+        uri = `${host}api/action/datastore_search?offset=${offset}&resource_id=${resourceId}&limit=${limit}&q=${zipCode}`;
+    }
+
     const options = {
         body: {},
         headers: {
@@ -30,12 +56,14 @@ app.get('/pantries/:zipCode', function(req, res) {
         },
         json: true,
         method: 'GET',
-        uri: `https://ckan.smartcolumbusos.com/api/action/datastore_search?resource_id=570a8e02-fb0e-4cee-895b-3b32bd740650&limit=100&q=${req.params.zipCode}`,
+        uri,
     };
     request(options, function(error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body);
+        if (error) {
+            console.log('error:', error); // Print the error if one occurred
+        }
+        // console.log('body:', body);
         res.send(body.result.records);
     });
 });
