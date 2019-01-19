0ee73f10cca934ca8421ea8c001db8ec10a68025
Eddie L <del.legg.iii@gmail.com>
2019-01-19 10:12:01 -0500

    Refactor to add unit tests

--- server.js
+++ server.js
@@ -1,119 +1,14 @@
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000;
-const request = require('request');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger.yaml');
-const host = 'https://ckan.smartcolumbusos.com/';
-const resourceId = '570a8e02-fb0e-4cee-895b-3b32bd740650';
-const queryHost = 'https://ckan.smartcolumbusos.com/api/3/action/datastore_search_sql?'
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// NOTE: query returns max of 100. to get next 100 we need to do the following query
-// '/api/action/datastore_search?offset=100&resource_id=570a8e02-fb0e-4cee-895b-3b32bd740650'
-app.get('/pantries', function(req, res) {
-    const limit = req.query.limit || 100; // query will only return 100 results.
-    const pageNumber = req.query.pageNumber || 1;
-
-    let uri = `${host}api/action/datastore_search?resource_id=${resourceId}&limit=${limit}`;
-    if (pageNumber > 1) {
-        const offset = limit * (pageNumber - 1);
-        uri = `${host}api/action/datastore_search?offset=${offset}&resource_id=${resourceId}&limit=${limit}`;
-    }
-
-    const options = {
-        body: {},
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        json: true,
-        method: 'GET',
-        uri,
-    };
-    request(options, function(error, response, body) {
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        if (error) {
-            console.log('error:', error); // Print the error if one occurred
-        }
-
-        // console.log('body:', body);
-        res.send(body.result.records);
-    });
-});
-// NOTE: query returns max of 100. to get next 100 we need to do the following query
-// '/api/action/datastore_search?offset=100&resource_id=570a8e02-fb0e-4cee-895b-3b32bd740650'
-app.get('/pantries/:zipCode', function(req, res) {
-    const limit = req.query.limit || 100;
-    const zipCode = req.params.zipCode;
-    const pageNumber = req.query.pageNumber || 1;
-
-    let uri = `${host}api/action/datastore_search?resource_id=${resourceId}&limit=${limit}&q=${zipCode}`;
-    if (pageNumber > 1) {
-        const offset = limit * (pageNumber - 1);
-        uri = `${host}api/action/datastore_search?offset=${offset}&resource_id=${resourceId}&limit=${limit}&q=${zipCode}`;
-    }
-
-    const options = {
-        body: {},
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        json: true,
-        method: 'GET',
-        uri,
-    };
-    request(options, function(error, response, body) {
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        if (error) {
-            console.log('error:', error); // Print the error if one occurred
-        }
-        // console.log('body:', body);
-        res.send(body.result.records);
-    });
-});
-
-app.get('/query/pantries', function(req, res) {
-    const currentlyActive = req.query.currentlyActive;
-    const showAccessible = req.query.handicapAccessible;
-    const limit = req.query.limit;
-
-    let uri = `${queryHost}sql=SELECT*FROM"570a8e02-fb0e-4cee-895b-3b32bd740650"`;
-    if (currentlyActive) {
-        const queryActive = currentlyActive === 'true';
-        uri = `${uri}WHERE"ACTIVE_FLAG"=${queryActive ? "'Y'" : "'N'"}`;
-    }
-
-    if (showAccessible) {
-        const queryAccessible = showAccessible === 'true';
-        uri = `${uri}${currentlyActive ? 'AND' : 'WHERE'}"HANDICAP_ACCESS"=${queryAccessible ? "'Y'" : "'N'"}`
-    }
-
-    if (limit) {
-        uri = `${uri}LIMIT'${limit}'`;
-    }
-
-    console.log(uri);
-    const options = {
-        body: {},
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        json: true,
-        method: 'GET',
-        uri,
-    };
-    request(options, function(error, response, body) {
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        if (error) {
-            console.log('error:', error); // Print the error if one occurred
-        }
-
-        // console.log('body:', body);
-        res.send(body.result.records);
-    });
-});
+const pantryRouter = require('./api/controllers/routes/pantries');
+app.use('/api/v1/pantries', pantryRouter);
 
 app.listen(port);
 
