07c43c13d543852630f77f5752a2e199de1d8dfa
Eddie L <del.legg.iii@gmail.com>
2019-01-31 19:19:23 -0500

    Create agency router

---
+++ api/controllers/routes/agency.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const requestUtils = require("../../utils/request");
+const queryUtils = require("../../utils/query");
+
+const env = process.env.NODE_ENV || 'development';
+
+const config = require("../../../config")[env];
+const host = config.host;
+const agencyResourceId = config.agency_resource;
+const agencyServiceResourceId = config.agency_service_resource;
+const serviceTaxonomyResourceId = config.service_taxonomy_resource;
+
+const uri = `${host}/api/3/action/datastore_search_sql?sql=SELECT * from "${agencyResourceId}" agency `;
+
+app.get("/", function (req, res) {
+    let query = '';
+    if (req.query.taxonomyId) {
+        query =
+            `
+            INNER JOIN "${agencyServiceResourceId}" agency_service ON agency."AGENCY_ID" = agency_service."AGENCY_ID" 
+            INNER JOIN "${serviceTaxonomyResourceId}" service_taxonomy ON agency_service."AGENCY_ID" = service_taxonomy."AGENCY_ID" 
+            AND agency_service."LINE_NUMBER" = service_taxonomy."LINE_NUMBER" 
+            WHERE service_taxonomy."TAXON_ID" = ${req.query.taxonomyId}
+        `;
+    }
+    requestUtils.sendRequest(queryUtils.getQueryString(req, uri + query), res);
+});
+
+app.get("/:id", function (req, res) {
+    requestUtils.sendRequest(uri + ` WHERE "AGENCY_ID" = ${req.params.id}`, res);
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -9,10 +9,12 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const pantryRouter = require('./api/controllers/routes/pantries');
 const taxonomyRouter = require('./api/controllers/routes/taxonomy');
+const agencyRouter = require('./api/controllers/routes/agency');
+
 
 app.use('/api/v1/pantries', pantryRouter);
 app.use('/api/v1/taxonomy', taxonomyRouter);
-
+app.use('/api/v1/agency', agencyRouter);
 
 app.listen(port);
 
