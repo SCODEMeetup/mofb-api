806ffddd738dd7eefe56183d4390f58347d3e906
yadavy <oclc*5972>
2019-06-09 21:49:16 -0400

    Fixing taxonomy and agency calls

--- api/controllers/services/ckan/locationService.js
+++ api/controllers/services/ckan/locationService.js
@@ -8,40 +8,38 @@ class LocationCkanService extends AbstractService {
         super("agency_location");
         this.agencyLocationResourceId = this.config.agency_location_resource;
         this.serviceLocationResourceId = this.config.service_location_resource;
-        this.uri = `${this.host}/api/v1/organization/handson_central_ohio/dataset/${this.agencyLocationResourceId}/query?_format=json`;
+        this.uri = `${this.host}/api/v1/organization/handson_central_ohio/dataset/`;
 
         this.queryFields = `
     ${this.tableName}."location_id", ${this.tableName}."location_number", ${this.tableName}."street_1", ${this.tableName}."street_2", ${this.tableName}."zip", ${this.tableName}."name",
     ${this.tableName}."phone_area_code", ${this.tableName}."phone_number", ${this.tableName}."phone_extension", ${this.tableName}."handicap_access",
     service_location."hours"
-`;
+    `;
         this.query = `
     SELECT ${this.queryFields} FROM "${this.agencyLocationResourceId}" ${this.tableName} 
     INNER JOIN "${this.serviceLocationResourceId}" service_location ON ${this.tableName}."agency_id" = service_location."agency_id"
     AND ${this.tableName}."location_id" = service_location."location_id"
-`;
+    `;
 
     }
 
     getAll(req, res) {
         let filter = '';
         let requestBody = '';
-        if (req.query.taxonomyId) {
+        /*if (req.query.taxonomyId) {
             requestBody = QueryUtils.joinTables;
             filter = getFilters(filter, `service_taxonomy."taxon_id" IN (${req.query.taxonomyId})`);
         }
         if (req.query.agencyId) {
             filter = getFilters(filter, `${this.tableName}."agency_id" IN (${req.query.agencyId})`);
-        }
-        requestBody = this.uri + this.query + requestBody;
-        const queryString = this.queryUtils.getQueryString(req, requestBody, filter, this.tableName);
-        this.requestUtils.getList(queryString, res, locationWithCoord(Location.get));
+        }*/
+        //requestBody = this.uri + this.query + requestBody;
+        const queryString = this.uri + `${this.serviceLocationResourceId}`+'/query?_format=json'+ this.queryUtils.setDefaultFilters('columns = location_id, location_number, street_1, street_2, zip, name, phone_area_code, phone_number, phone_extension, handicap_access')
+        console.log(this.requestUtils.getList(queryString, res, locationWithCoord(Location.get)));
     }
 
     get(req, res) {
-        let requestBody = this.query + QueryUtils.joinTables;
-        const queryString = this.uri + requestBody +
-            this.queryUtils.setDefaultFilters(`${this.tableName}."location_id" = ${req.params.id} AND service_taxonomy."taxon_id" = ${req.params.serviceId}`, this.tableName);
+        const queryString = this.uri + this.queryUtils.setDefaultFilters('and columns = location_id, location_number, street_1, street_2, zip, name, phone_area_code, phone_number, phone_extension, handicap_access')
         this.requestUtils.getObject(queryString, res, locationWithCoord(Location.get));
     }
 }
