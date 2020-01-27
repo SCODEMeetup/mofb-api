ce495ef83dda89b37df1a90123ccfb2ba478607b
mollyhenderson <henderson.molly4@gmail.com>
2020-01-26 21:31:27 -0500

    Actually handle the data correctly.

--- api/services/taxonomyService.ts
+++ api/services/taxonomyService.ts
@@ -2,6 +2,7 @@ import { makeSCOSRequest } from './scosService';
 import getLogger from '../utils/logger';
 import TaxonomyDto from '../models/dto/taxonomyDto';
 import ScosSubcategoryDto from '../models/scosApi/scosSubcategoryDto';
+import ScosCategoryDto from '../models/scosApi/scosCategoryDto';
 
 const { CATEGORIES_TABLE } = process.env;
 
@@ -9,14 +10,18 @@ const log = getLogger('taxonomyService');
 
 async function getSubcategories(id: string): Promise<TaxonomyDto[]> {
   log.debug(`Getting subcategories of category id ${id}`);
-  const response: ScosSubcategoryDto[] = await makeSCOSRequest(`SELECT subcat FROM ${CATEGORIES_TABLE} WHERE categoryid = '${id}'`);
-  return response.map((subcat: ScosSubcategoryDto): TaxonomyDto => ({
+
+  const query = `SELECT subcat FROM ${CATEGORIES_TABLE} WHERE categoryid = '${id}'`;
+  const response: Pick<ScosCategoryDto, 'subcat'>[] = await makeSCOSRequest(query);
+
+  return response[0].subcat.map((subcat: ScosSubcategoryDto): TaxonomyDto => ({
     id: subcat.subcategoryid,
     description: subcat.subcategory,
     parentCategoryId: id,
     // TODO: hardcoded bc I'm not sure if this is still available in the data
     level: 1,
-  }));
+  }
+  ));
 }
 
 export { getSubcategories };
